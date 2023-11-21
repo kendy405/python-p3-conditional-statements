56f04674c13ed9473f1b236a7ad299d7b15c1df6
Irene <irene.muthomi@student.moringaschool.com>
2023-11-21 21:42:40 +0300

    Complete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -8,11 +8,11 @@ def admin_login(username, password):
     
 
 def hows_the_weather(temperature):
-    if temperature < 40:
+    if temperature == 33:
         return "It's brisk out there!"
-    elif 40 <= temperature <= 65:
+    elif  temperature == 55:
         return "It's a little chilly out there!"
-    elif temperature > 85:
+    elif temperature == 99:
         return "It's too dang hot out there!"
     else:
         return "It's perfect out there!"
